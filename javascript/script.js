3321909b518d5b0c6fe15af2ee06f918df48f058
Stacey Muscat <staceymuscatgmail.com>
2021-02-22 22:09:42 +1100

    Create function that assigns a random colour to each div as it is passed

--- javascript/script.js
+++ javascript/script.js
@@ -1,4 +1,3 @@
-
 function changeGrid() {
   let promptSize = prompt("How many rows do you want?");
 
@@ -8,28 +7,40 @@ function changeGrid() {
           }
   //console.log(promptSize);
   createGrid(promptSize);
+}
 
+let random = '';
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
 
 
-  function createGrid(numberOfRows) {
-    var Container = document.getElementById("container");
-    Container.innerHTML = '';
 
-    //let numberOfRows = prompt("How many rows do you want?");
-    //let i = 0;
-    let x = numberOfRows * numberOfRows;
+function createGrid(numberOfRows) {
+  var Container = document.getElementById("container");
+  Container.innerHTML = '';
 
-    document.documentElement.style.setProperty("--columns-row", numberOfRows);
-    for (i =  0; i < x ; i++) {
-      var div = document.createElement("div");
-      div.className = "grid";
-      document.getElementById("container").appendChild(div);
+  let x = numberOfRows * numberOfRows;
 
-      div.addEventListener("mouseenter", function () {
-      this.style.backgroundColor = "black";
+  document.documentElement.style.setProperty("--columns-row", numberOfRows);
+  for (i =  0; i < x ; i++) {
+    var div = document.createElement("div");
+    div.className = "grid";
+    document.getElementById("container").appendChild(div);
+
+    div.addEventListener("mouseenter", function () {
+    this.style.backgroundColor = getRandomColor();
     });
   }
 }
 
+getRandomColor();
 createGrid(16);
+
+console.log(random);
